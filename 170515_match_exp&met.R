2ffcd37a477f4e1e95396251b0fa3997602368ed
rg-code <rgupta6@ucmerced.edu>
2018-02-26 12:40:05 -0800

    updates

--- 170515_match_exp&met.R
+++ 170515_match_exp&met.R
@@ -1,23 +1,25 @@
 i = 0
 j = 0
-samples.info <- colData(data1)
+samples.info <- colData(met)
 samples.info$newCol <- NA
-for (i in 1:length(testlo1))
+low.quantile.samples <- substr(low.quantile.samples,start = 1, stop = 12)
+for (i in 1:length(low.quantile.samples))
 {
-  for (j in 1:length(data1$"patient"))
+  for (j in 1:length(met$"patient"))
   {
-    if ((grepl(testlo1[i], data1$"patient"[j])) == TRUE)
+    if ((grepl(low.quantile.samples[i], met$"patient"[j])) == TRUE)
     {
       samples.info$newCol[j] <- "DNMT1.low"
   
     }
   }
 }
-for (i in 1:length(testhigh))
+high.quantile.samples <- substr(high.quantile.samples,start = 1, stop = 12)
+for (i in 1:length(high.quantile.samples))
 {
-  for (j in 1:length(data1$"patient"))
+  for (j in 1:length(met$"patient"))
   {
-    if ((grepl(testhigh[i], data1$"patient"[j])) == TRUE)
+    if ((grepl(high.quantile.samples[i], met$"patient"[j])) == TRUE)
     {
       samples.info$"newCol"[j] <- "DNMT1.high"
     }
@@ -26,9 +28,9 @@ for (i in 1:length(testhigh))
 mid.quantile.samples <- substr(mid.quantile.samples,start = 1, stop = 12)
 for (i in 1:length(mid.quantile.samples))
 {
-  for (j in 1:length(data1$"patient"))
+  for (j in 1:length(met$"patient"))
   {
-    if ((grepl(mid.quantile.samples[i], data1$"patient"[j])) == TRUE)
+    if ((grepl(mid.quantile.samples[i], met$"patient"[j])) == TRUE)
     {
       samples.info$"newCol"[j] <- "DNMT1.mid"
     }
